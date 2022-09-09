fb7d6df709de09490ff48c724163e9a295583eb8
sjquant <seonujang92@gmail.com>
2022-09-09 21:59:26 +0900

    🚧 Copilot did everything...

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import Todo from "./pages/Todo.vue";
 </script>
 
 <template>
-  <HelloWorld />
+  <Todo />
 </template>
---
+++ src/pages/Todo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="todo">
+    <h1>Todo</h1>
+    <input type="text" v-model="newTodo" @keyup.enter="addTodo" />
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const newTodo = ref("");
+const todos = ref(["Learn Vue 3", "Learn Vite"]);
+
+function addTodo() {
+  todos.value.push(newTodo.value);
+  newTodo.value = "";
+}
+</script>
